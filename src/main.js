c80f0ef82718d7615c292671b244ea5ba0f3b7b3
frllk <1402805300@qq.com>
2020-07-03 08:35:43 +0800

    安装vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'element-ui'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
