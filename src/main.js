bd8596f948330257809f56aa9e0e96c3b7f0ce3c
frllk <1402805300@qq.com>
2020-07-03 21:07:52 +0800

    安装vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'element-ui'
 import router from './krouter'
+import store from './store'
 // import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +11,9 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 new Vue({
-  router, // 设置router,它怎么起作用的呢??????
+  // 设置router,它怎么起作用的呢??????
+  router,
+
+  store,
   render: h => h(App)
 }).$mount('#app')
