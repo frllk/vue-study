fe552a230c8684d551eb323414711686bdd2ba98
frllk <1402805300@qq.com>
2020-07-18 01:16:12 +0800

    状态管理：vuex-module-decorators

--- ts/src/App.vue
+++ ts/src/App.vue
@@ -3,6 +3,9 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld @add-feature="addFeature" msg="Welcome to Your Vue.js + TypeScript App" />
     <!-- <TsxComp msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <hr>
+    <p @click="add">{{$store.state.counter.count}}</p>
+    <p @click="asyncAdd">{{count}}</p>
   </div>
 </template>
 
@@ -11,6 +14,7 @@ import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
 import TsxComp from './components/TsxComp'
 import { FeatureSelect } from './types'
+import CounterModule from './store/counter'
 
 @Component({
   components: {
@@ -21,6 +25,18 @@ import { FeatureSelect } from './types'
 export default class App extends Vue {
   addFeature(feature: FeatureSelect) {
     console.log('添加了一个新特性', feature.name)
+  }
+
+  get count() {
+    return CounterModule.count
+  }
+
+  add() {
+    CounterModule.add()
+  }
+
+  asyncAdd() {
+    CounterModule.asyncAdd()
   }
 }
 </script>
